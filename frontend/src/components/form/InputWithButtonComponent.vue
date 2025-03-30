bbc2a8ac66020b5895a1d997cc44c50db36292c8
Queue-Evgenii <genadolgii228@gmail.com>
2025-03-30 15:17:44 +0300

    ADD new task control markup

---
+++ frontend/src/components/form/InputWithButtonComponent.vue
@@ -0,0 +1,24 @@
+tem<script setup lang="ts">
+import InputComponent, { type InputProps } from './InputComponent.vue';
+
+const value = defineModel<string>({
+  required: true,
+});
+defineProps<InputProps>();
+const emits = defineEmits(["clickHoisting"])
+const handleClick = () => {
+  emits("clickHoisting", value.value);
+}
+</script>
+
+<template>
+  <div class="input-with _flex _f-dir-col _gap-y-8">
+    <InputComponent
+      v-model="value"
+      :placeholder="placeholder"
+    />
+    <button type="button" @click="handleClick" class="input-with__button">
+      <slot></slot>
+    </button>
+  </div>
+</template>
--- frontend/src/views/home/TasksView.vue
+++ frontend/src/views/home/TasksView.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import type { TaskApi } from '@/api/modules/Task';
+import InputWithButtonComponent from '@/components/form/InputWithButtonComponent.vue';
 import TaskListComponent from '@/components/TaskListComponent.vue';
 import { useTaskListStore } from '@/stores/tasks';
-import { inject } from 'vue';
+import { inject, ref } from 'vue';
 
 const taskListStore = useTaskListStore();
-
+const newTask = ref("");
+const addNewTask = () => {
+  console.log(newTask.value)
+}
 </script>
 
 <template>
@@ -30,6 +34,19 @@ const taskListStore = useTaskListStore();
         class="tasks__section tasks__overdued"
       />
     </div>
+    <div class="tasks__input _container">
+      <InputWithButtonComponent
+        v-model="newTask"
+        placeholder="Enter a new task"
+        @click-hoisting="addNewTask"
+        class="_input"
+      >
+        <div class="_flex _ai-c _jc-c _gap-x-8">
+          <span>Add</span>
+          <i class="material-icons">add</i>
+        </div>
+      </InputWithButtonComponent>
+    </div>
   </div>
 </template>
 
@@ -39,5 +56,10 @@ const taskListStore = useTaskListStore();
   width: 100%;
   height: 1px;
   background-color: var(--color-background-mute);
+}
+.tasks__input {
+  position: fixed;
+  width: 100%;
+  bottom: 70px;
 }
 </style>
